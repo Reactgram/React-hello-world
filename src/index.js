ce11c7abea336e40c9959b67ba642e66e89fc87c
Instructor <dodydev311@gmail.com>
2024-07-17 19:58:42 +0530

    basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+// v-17
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// modules 
+import App from './App.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// functional component: UI:  => HTML
+
+
+
+
+
+// virtualDOM
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
+
+
+
+
+
+
+
+
+
+// function hello(){
+//     return "Hello World"
+// }
+
+// hello()
+
+
+// document.getElementById("root")
